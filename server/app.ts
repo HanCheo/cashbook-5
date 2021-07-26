e1ee7ebe606b0bbd5f7879d38827ceb0a6438340
peanut-lover <jihoson94@gmail.com>
2021-07-26 20:54:10 +0900

    chore: BE Express 개발환경 세팅

---
+++ server/app.ts
@@ -0,0 +1,34 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import path from 'path';
+import * as http from 'http';
+import router from './src/api';
+
+const port = process.env.PORT || 3000;
+
+const app: Application = express();
+
+const httpServer = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use(router);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const err = new Error('Not Found') as any;
+  err.status = 404;
+  next(err);
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500).json({
+    error: err.message,
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server Port: ${port}`);
+});
