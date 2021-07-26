6e0c1023e6720359233d179b2d99e89c1c3d47f0
김한철 <38929712+HanCheo@users.noreply.github.com>
2021-07-26 21:20:43 +0900

    [#4] feat : 기본 헤더 양식 추가
    1. html에 vw에 따른 font-size 추가
    2. 헤더 기본 scss 양식 추가

--- client/src/components/header/header.ts
+++ client/src/components/header/header.ts
@@ -4,13 +4,22 @@ import './header.scss';
 
 export default class Header extends Component {
   template() {
-    return `
-        <div class="hader-wrap">
-          <div class="title">가계부</div>
-          <div class="month">${this.$state.date}</div>
-          <div class="icons">아이콘</div>
-        </div>
-      `;
+    return /*html*/ `
+      <div class="hader-wrap">
+        <ul>
+          <li class="left">우아한 가계부</li>
+          <li class="center">
+            <div class="arrow"><</div>
+            <div class="date-wrap">
+              <div class="month">7월</div>
+              <div class="year">2021</div>
+            </div>
+            <div class="arrow">></div>
+          </li>
+          <li class="right">아이콘</li>
+        </ul>
+      </div>
+    `;
   }
 
   mounted() {
