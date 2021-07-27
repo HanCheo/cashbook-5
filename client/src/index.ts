9a04586b24309b57cdf68a1db92910a74bbef948
김한철 <38929712+HanCheo@users.noreply.github.com>
2021-07-27 11:56:52 +0900

    [#4] fix : Header Import 수정

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,5 @@
 import './normalize.css';
-import Header from './components/header/header';
+import Header from './components/Header/';
 import { initRouter } from './core/router';
 import { Main } from './pages';
 
