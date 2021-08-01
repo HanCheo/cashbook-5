11c531d8714e49ab59fa1d794c0845ad1e8800b7
김한철 <05.hanch.it@gmail.com>
2021-08-01 14:52:34 +0900

    fix : 리뷰 적용

--- server/src/models/Account/AbstractAccountsStore.ts
+++
@@ -1,6 +0,0 @@
-export default class AbstractAccountsStore {
-  close() {}
-  create(id: string, name: string) {}
-  get(id: string) {}
-  delete(id: string) {}
-}
--- server/src/oAuth/git.oauth.ts
+++ server/src/oAuth/git.oauth.ts
@@ -13,47 +13,49 @@ type GithubUserResponse = {
   avatar_url: string;
 };
 
-// getUserInfo 리턴값
-// {
-//   login: 'HanCheo',
-//   id: number,
-//   node_id: string,
-//   avatar_url: 'https://avatars.githubusercontent.com/u/38929712?v=4',
-//   gravatar_id: '',
-//   url: 'https://api.github.com/users/HanCheo',
-//   html_url: 'https://github.com/HanCheo',
-//   followers_url: 'https://api.github.com/users/HanCheo/followers',
-//   following_url: 'https://api.github.com/users/HanCheo/following{/other_user}',
-//   gists_url: 'https://api.github.com/users/HanCheo/gists{/gist_id}',
-//   starred_url: 'https://api.github.com/users/HanCheo/starred{/owner}{/repo}',
-//   subscriptions_url: 'https://api.github.com/users/HanCheo/subscriptions',
-//   organizations_url: 'https://api.github.com/users/HanCheo/orgs',
-//   repos_url: 'https://api.github.com/users/HanCheo/repos',
-//   events_url: 'https://api.github.com/users/HanCheo/events{/privacy}',
-//   received_events_url: 'https://api.github.com/users/HanCheo/received_events',
-//   type: 'User',
-//   site_admin: false,
-//   name: 'HanCh',
-//   company: null,
-//   blog: '',
-//   location: null,
-//   email: null,
-//   hireable: null,
-//   bio: null,
-//   twitter_username: null,
-//   public_repos: 13,
-//   public_gists: 0,
-//   followers: 8,
-//   following: 3,
-//   created_at: '2018-05-03T01:27:47Z',
-//   updated_at: '2021-07-31T06:32:25Z'
+/**
+getUserInfo 리턴값
+{
+  login: 'HanCheo',
+  id: number,
+  node_id: string,
+  avatar_url: 'https://avatars.githubusercontent.com/u/38929712?v=4',
+  gravatar_id: '',
+  url: 'https://api.github.com/users/HanCheo',
+  html_url: 'https://github.com/HanCheo',
+  followers_url: 'https://api.github.com/users/HanCheo/followers',
+  following_url: 'https://api.github.com/users/HanCheo/following{/other_user}',
+  gists_url: 'https://api.github.com/users/HanCheo/gists{/gist_id}',
+  starred_url: 'https://api.github.com/users/HanCheo/starred{/owner}{/repo}',
+  subscriptions_url: 'https://api.github.com/users/HanCheo/subscriptions',
+  organizations_url: 'https://api.github.com/users/HanCheo/orgs',
+  repos_url: 'https://api.github.com/users/HanCheo/repos',
+  events_url: 'https://api.github.com/users/HanCheo/events{/privacy}',
+  received_events_url: 'https://api.github.com/users/HanCheo/received_events',
+  type: 'User',
+  site_admin: false,
+  name: 'HanCh',
+  company: null,
+  blog: '',
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 13,
+  public_gists: 0,
+  followers: 8,
+  following: 3,
+  created_at: '2018-05-03T01:27:47Z',
+  updated_at: '2021-07-31T06:32:25Z'
 
-// getAccessInfo 리턴값
-// } {
-//   access_token: 'access_token',
-//   token_type: 'bearer',
-//   scope: ''
-// }
+getAccessInfo 리턴값
+} {
+  access_token: 'access_token',
+  token_type: 'bearer',
+  scope: ''
+} 
+*/
 
 const OAUTH_GIT_URL = 'https://github.com/login/oauth';
 const API_GIT_URL = 'https://api.github.com/user';
