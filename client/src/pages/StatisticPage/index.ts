b592e2aa5077235ac62c354448ee6fabca3e359b
손지호 <sonjiho@sonjihoui-MacBookPro.local>
2021-08-03 21:33:25 +0900

    feat: 헤더 월 변경에 의한 그래프 리렌더링

--- client/src/pages/StatisticPage/index.ts
+++ client/src/pages/StatisticPage/index.ts
@@ -7,6 +7,9 @@ import PieChart, { PieChartData } from '@/src/utils/charts/PieChart';
 import CategoryList, { CategoryItem } from './CategoryList';
 import { removeAllChildNode } from '@/src/utils/domHelper';
 import './index.scss';
+import calendarDataModel from '@/src/models/Calendar';
+
+const CALENDAR_OBSERVER_LISTENER_KEY = "statistic"
 
 interface IState {
   statisticData?: StatisticLedgerByCategory;
@@ -16,9 +19,14 @@ interface IProps { }
 
 export default class StatisticPage extends Component<IState, IProps> {
   template() {
+    const { statisticData } = this.$state;
     return /* html */`
             <div class='statistic-container'>
               <div class="chart-container">
+                ${!statisticData || Object.keys(statisticData).length === 0
+        ? html`
+                  <h1>데이터가 없습니다.</h1>
+                  ` : ""}
                 <div id="pie-chart"></div>
                 <div id="statistic-category-container"></div>
               </div>
@@ -35,7 +43,8 @@ export default class StatisticPage extends Component<IState, IProps> {
   setup() {
     this.$state = { statisticData: {} };
 
-    getStatisticLedgers(new Date()).then(result => {
+    const calendarDate = calendarDataModel.getDate();
+    getStatisticLedgers(calendarDate).then(result => {
       if (result.success) {
         const statisticData = result.data;
 
@@ -72,12 +81,9 @@ export default class StatisticPage extends Component<IState, IProps> {
   renderPieChart() {
     const { statisticData } = this.$state;
     const $pieChartContainer = document.querySelector('#pie-chart') as HTMLElement;
-    if (!statisticData) {
-      $pieChartContainer.innerHTML = html`<p>데이터가 없습니다.</p>`;
-    } else {
+    if (statisticData) {
       const pieChartData = mapToPieChartData(statisticData);
       removeAllChildNode($pieChartContainer);
-
       const $pieChartSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg") as SVGElement;
       $pieChartContainer.appendChild($pieChartSVG);
       PieChart.init($pieChartSVG, pieChartData, {
@@ -93,9 +99,7 @@ export default class StatisticPage extends Component<IState, IProps> {
     const $lineChartContainer = document.querySelector('#line-chart') as HTMLElement;
     removeAllChildNode($lineChartContainer);
 
-    if (!statisticData) {
-      $lineChartContainer.innerHTML = html`<p>데이터가 없습니다.</p>`;
-    } else {
+    if (statisticData) {
       const lineChartData = mapToLineChartData(statisticData)
       this.renderLineChart(lineChartData);
     }
@@ -106,9 +110,7 @@ export default class StatisticPage extends Component<IState, IProps> {
     const $lineChartContainer = document.querySelector('#line-chart') as HTMLElement;
     removeAllChildNode($lineChartContainer);
 
-    if (!statisticData) {
-      $lineChartContainer.innerHTML = html`<p>데이터가 없습니다.</p>`;
-    } else {
+    if (statisticData) {
       let categortAsKey: keyof StatisticLedgerByCategory = category;
       const filtered: StatisticLedgerByCategory = {
         [category]: statisticData[categortAsKey]
@@ -124,6 +126,28 @@ export default class StatisticPage extends Component<IState, IProps> {
     LineChart.init($lineChartSVG, lineChartData);
     $lineChartContainer.appendChild($lineChartSVG);
   }
+
+  async CalendarModelSubscribeFunction() {
+    const calendarDate = calendarDataModel.getDate();
+    getStatisticLedgers(calendarDate).then(result => {
+      if (result.success) {
+        const statisticData = result.data;
+
+        this.setState({
+          statisticData: statisticData,
+        });
+      }
+    });
+  }
+
+  setUnmount() {
+    calendarDataModel.unsubscribe(CALENDAR_OBSERVER_LISTENER_KEY);
+  }
+  setEvent() {
+    calendarDataModel.subscribe(CALENDAR_OBSERVER_LISTENER_KEY, this.CalendarModelSubscribeFunction.bind(this));
+    this.resetEvent();
+  }
+
 }
 
 function mapToCategoryItemData(data: StatisticLedgerByCategory): CategoryItem[] {
