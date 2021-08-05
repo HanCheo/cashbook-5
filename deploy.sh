f1ed47df923de900b81827928c1531d61d2ce6bf
peanut-lover <jihoson94@gmail.com>
2021-08-05 15:27:09 +0900

    feat: 자동 배포 스크립트 추가

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+git fetch
+
+# Current Branch Name
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+echo BRANCH: $BRANCH
+
+# Current Commit Hash
+CURRENT_HASH=$(git rev-parse HEAD)
+echo CURRENT : $CURRENT_HASH
+
+# Remote Commit Hash
+REMOTE_HASH=$(git rev-parse origin/$BRANCH)
+echo REMOTE : $REMOTE_HASH
+
+if [[ "$CURRENT_HASH" != "$REMOTE_HASH" ]];  then
+	echo "Sync with Git Repository"
+	git pull origin $BRANCH --rebase
+
+	echo "Start Client Deploy"
+	cd ./client
+    npm install
+    npm run build
+	mv ./dist/* /var/www/html
+	echo "Client Deployed"
+
+	echo "Start Server Deploy"
+	cd ../server
+    npm install
+	npm run build
+	npm run prod-start
+	echo "Server Deployed"
+else
+	echo $(date)
+	echo "변경 사항이 없습니다".
+fi
+
